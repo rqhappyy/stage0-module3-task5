94f3d78f295188e548edb8a3fbca8f20764cab3f
rqhappyy <dtereschenko110@gmail.com>
2023-11-08 11:52:10 +0300

    task6

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    static void divide(int toBeDivided) {
+        int result = toBeDivided/5;
+        System.out.println(result);
     }
 }
